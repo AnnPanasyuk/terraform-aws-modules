b22b4825d0e9fb011d933de62fb994dca4a39899
Ann Panasyuk <30906983+AnnPanasyuk@users.noreply.github.com>
2020-05-22 16:58:46 +0200

    example.tf
    updated

--- tf-module/example.tf
+++ tf-module/example.tf
@@ -1,20 +1,19 @@
 provider "aws" {
-  profile    = "default"
-  region     = "us-east-1"
+ profile  = "default"
+ region   = var.region
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-2757f631"
-  instance_type = "t2.micro"
+ ami          = "ami-2757f631"
+ instance_type     = "t2.micro"
+ vpc_security_group_ids = ["sg-24319f5f"]
+ subnet_id       = "subnet-41b49e24"
+ provisioner "local-exec" {
+  command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+ }
 }
-
 
 resource "aws_eip" "ip" {
-    vpc = true
-    instance = aws_instance.example.id
-}
-
-
-output "ip" {
-  value = aws_eip.ip.public_ip
+  vpc = true
+  instance = aws_instance.example.id
 }

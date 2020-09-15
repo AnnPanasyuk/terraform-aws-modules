e376a10487e18e05dd5d6a94b0c6851a3af36c00
Ann Panasyuk <a.panasyuk@scalr.com>
2020-09-15 15:30:37 +0300

    created new terraform module folder

---
+++ folder1/terraform-aws-module1/example.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+ profile  = "default"
+ region   = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+ ami          = "ami-2757f631"
+ instance_type     = "t2.micro"
+ vpc_security_group_ids = ["sg-24319f5f"]
+ subnet_id       = "subnet-41b49e24"
+ provisioner "local-exec" {
+  command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+ }
+}
+
+resource "aws_eip" "ip" {
+  vpc = true
+  instance = aws_instance.example.id
+}

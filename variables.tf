1e7b58283a8c80905a587b4a07580ee184230b0f
Ann Panasyuk <30906983+AnnPanasyuk@users.noreply.github.com>
2021-09-10 15:27:30 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,30 @@
 variable "my_variable" {
   default = null
 }
+
+variable "region" {
+  default = "us-east-2"
+}
+
+variable "docker_ports" {
+  type = list(object({
+    internal = number
+    external = number
+    protocol = string
+  }))
+  default = [
+    {
+      internal = 8300
+      external = 8300
+      protocol = "tcp"
+    }
+  ]
+}
+
+variable "some_value" {
+  default = ""
+}
+
+variable "some_value2" {
+  default = "null"
+}

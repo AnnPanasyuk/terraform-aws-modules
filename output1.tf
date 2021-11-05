7a50bdbbb0c999878d7f11a64d36aa0925bff116
Ann Panasyuk <30906983+AnnPanasyuk@users.noreply.github.com>
2021-11-05 15:09:51 +0200

    Update output1.tf

--- output1.tf
+++ output1.tf
@@ -2,3 +2,29 @@ output "ip" {
   value = aws_eip.ip.public_ip
   sensitive = true
 }
+
+
+output "outputNullStr" {
+  value = var.some_value2
+  sensitive = false
+}
+
+output "outputEmptyStr" {
+  value = var.some_value
+  sensitive = false
+}
+
+output "outputRegion" {
+  value = var.region
+  sensitive = false
+}
+
+output "outputNull" {
+  value = var.some_null
+  sensitive = false
+}
+
+output "outputEmpty" {
+  value = var.empty
+  sensitive = false
+}
